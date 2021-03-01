af781be2cc1f9e8f4953103a31e58ad8db40da24
sv549 <72826068+sv549@users.noreply.github.com>
2021-03-01 10:18:16 -0500

    Update decorators.py

--- Decorators/decorators.py
+++ Decorators/decorators.py
@@ -1,6 +1,7 @@
 def decorator(func):
-    def decorator_do_twice():
-        func()
-        func()
+    def decorator_do_twice(a,b):
+        if a < 0 and b < 0:
+            print("Must be a number greater than 0")
+        
 
         return decorator_do_twice()
